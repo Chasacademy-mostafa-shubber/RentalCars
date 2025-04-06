70f16386bea732b26b673770739c93f7714ecc9c
Chasacademy-mostafa-shubber <113859196+Chasacademy-mostafa-shubber@users.noreply.github.com>
2025-04-06 23:55:36 +0200

    navbar

--- RentalCars/wwwroot/js/site.js
+++ RentalCars/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//$(document).ready(function () {
+//    $(".navbar-toggler").click(function () {
+//        $(".navbar").toggleClass("navbar-collapse-background");
+//        $(".nav-link").toggleClass("navlink-collapse-color");
+//        $(".navbar-toggler-icon").toggleClass("navbaricon-collapse-color");
+//    });
+//});
