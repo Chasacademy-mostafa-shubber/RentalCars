99de7154a24fcc05232670027751a75669acfac7
Chasacademy-mostafa-shubber <113859196+Chasacademy-mostafa-shubber@users.noreply.github.com>
2025-06-17 12:55:13 +0200

    form

--- RentalCars/Controllers/HomeController.cs
+++ RentalCars/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using RentalCars.Data;
 using RentalCars.Models;
 
 namespace RentalCars.Controllers;
@@ -7,16 +8,23 @@ namespace RentalCars.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        ViewData["NavTitle"] = "Home";
+        
         return View();
+    }
+
+    public IActionResult Cars()
+    {
+        return View(_context.Cars.ToList());
     }
 
     public IActionResult Privacy()
